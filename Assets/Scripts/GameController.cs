74d46b3c4adc7b9cb85788f407046870637d91a0
amhsiung1613 <133808504+amhsiung1613@users.noreply.github.com>
2024-12-05 13:18:14 -0800

    enemy ai done, level 2 done, need antigravity

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,6 +9,11 @@ public class GameController : MonoBehaviour
     int progressAmount;
     public Slider progressSlider;
 
+    public GameObject player;
+    public GameObject LoadCanvas;
+    public List<GameObject> levels;
+    private int currentLevelIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +21,7 @@ public class GameController : MonoBehaviour
         progressSlider.value = 0;
         MachineParts.OnGearCollect += IncreaseProgressAmount;
         HoldToLoadLevel.OnHoldComplete += LoadNextLevel;
+        LoadCanvas.SetActive(false);
     }
 
     void IncreaseProgressAmount(int amount) {
@@ -23,12 +29,23 @@ public class GameController : MonoBehaviour
         progressSlider.value = progressAmount;
         if (progressAmount >= 100) {
             //level finished
+            LoadCanvas.SetActive(true);
             Debug.Log("level complete");
         }
     }
 
     void LoadNextLevel() {
+        int nextLevelIndex = (currentLevelIndex == levels.Count - 1) ? 0 : currentLevelIndex + 1;
+        LoadCanvas.SetActive(false);
 
+        levels[currentLevelIndex].gameObject.SetActive(false);
+        levels[nextLevelIndex].gameObject.SetActive(true);
+
+        player.transform.position = new Vector3(0, 0, 0);
+
+        currentLevelIndex = nextLevelIndex;
+        progressAmount = 0;
+        progressSlider.value = 0;
     }
 
     // Update is called once per frame
